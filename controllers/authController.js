01a30e2a2294e50528d9441dfccf3a3b833c2746
rolarblaze <rolarblaze@github.com>
2023-03-13 02:32:57 +0100

    signup and login added to the commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,20 +2,20 @@ const User = require("../models/userModels")
 
 const bcrypt = require("bcryptjs")
 
-exports.signUp = async (req, res, netx) => {
+exports.signUp = async (req, res) => {
     const {username, password} = req.body
-    const hashpassword = await bcrypt.hash(password, 12)
+    const hashpassword = await bcrypt.hash(password, 12);
     try {
         const newUser = await User.create({
             username,
             password: hashpassword
-        })
+        });
         res.status(201).json({
             status: "success",
             data: {
                 user: newUser
             }
-        })
+        });
     } catch (e) {
         res.status(402).json({
             status: "fail"
@@ -25,4 +25,27 @@ exports.signUp = async (req, res, netx) => {
 
 exports.login = async (req, res) => {
     const {username, password} = req.body
+    try {
+        const user = await User.findOne({username})
+        if(!user){
+            return res.status(402).json({
+                status: "fail",
+                message: "user not found"
+            });
+        }
+        const isCorrect = await bcrypt.compare(password, user.password)
+        if (isCorrect) {
+            res.status(200).json({status: "success"})
+        } else {
+            res.status(402).json({status: "fail", message: "incorrect password!"})
+        }
+        const newUser = await User.create({
+            username,
+            password: hashpassword
+        });
+    } catch (e) {
+        res.status(402).json({
+            status: "fail"
+        });
+    }
 }
