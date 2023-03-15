074d5884eeca78e4a1e606dde1a1c9eea494cd3e
rolarblaze <rolarblaze@github.com>
2023-03-15 13:38:56 +0100

    changes made to middleware  file

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,16 +1,17 @@
 const express = require("express");
 
 const postController = require("../controllers/postController");
+const protect = require("../middleware/authMiddleWare");
 
 const router = express.Router();
 
 // this route to locaalhost 3000
 router
     .route("/")
-    .get(postController.getAllPosts)
-    .post(postController.createPost);
+    .get(protect, postController.getAllPosts)
+    .post(protect, postController.createPost);
 router
     .route("/:id")
-    .get(postController.getOnePost)
-    .patch(postController.updatePost)
-    .delete(postController.updatePost);
+    .get(protect, postController.getOnePost)
+    .patch(protect, postController.updatePost)
+    .delete(protect, postController.updatePost);
